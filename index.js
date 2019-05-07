86c5d2c1acb5ef3d71c1373ed42641f37da438f6
dennis wang <dwang0816@gmail.com>
2019-05-07 22:19:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function dwarfRollCall(dwarves) {
+  dwarves = ["Dopey", "Grumpy", "Bashful"]
+  for (i = 0; i < dwarves.length; i++)
+  console.log(i+1 + "." + dwarves[i])
+    
 }
+  
 
 function summonCaptainPlanet(planeteerCalls){
 }
@@ -9,3 +14,9 @@ function longPlaneteerCalls(words) {
 
 function findTheCheese (foods) {
 }
+
+
+
+
+
+
